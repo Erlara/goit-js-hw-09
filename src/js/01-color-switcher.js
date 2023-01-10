61471be80e8811a38f1ff2ccac0717c37955c81c
Larisa Melnichenko <erlara@ukr.net>
2023-01-10 11:36:36 +0200

    change

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,6 @@
 
+const startEl = document.querySelector('[data-start]');
+const stopEl = document.querySelector('[data-stop]');
+
+//startEl.addEventListener("click", startColor);
+//stopEl.addEventListener("click", stopColor);
